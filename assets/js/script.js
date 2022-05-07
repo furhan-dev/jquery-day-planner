05a3ff1970aef23c71aa24d7f041b10a11a602ba
Furhan Samani <contact@furhan.dev>
2022-05-07 01:13:54 -0700

    check in MVP

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ function init() {
 }
 
 function createSchedule() {
-
     // set the number of hours in a work day (8 + 1 hour for lunch)
     const workHours = 9;
     // create a variable to keep track of current hour in for loop
@@ -21,11 +20,11 @@ function createSchedule() {
     // loop over all work hours and add row
     for (let i = 0; i < workHours; i++) {
         // create a row
-        let rowEl = $('<tr>').addClass('row');
+        let rowEl = $('<tr>').addClass('row time-block');
         // create hour td
         let hourTdEl = $('<td>').addClass('hour col-2 col-md-1').text(currentHour.format('hA'));
 
-        // create desc container td
+        // create desc container td with a data-time attribute to be set to the currentHour in unix time
         let descTdEl = $('<td>').addClass('col-8 col-md-10 p-0');
         // color background based on current hour
         if (currentHour.isBefore(rightNow, 'hour')) {
@@ -37,11 +36,17 @@ function createSchedule() {
         }
 
         // create desc text area element
-        let descEL = $('<textarea>').addClass('description');
-        descTdEl.append(descEL);
+        let descEl = $('<textarea>').addClass('description').attr('data-time', currentHour.format('YYYY-MM-DD HH'));
+        // set the description if previously saved
+        const desc = localStorage.getItem(currentHour.format('YYYY-MM-DD HH'));
+        if (desc != null) {
+            descEl.text(desc);
+        }
+
+        descTdEl.append(descEl);
         // create save button td
         let saveTdEl = $('<td>').addClass('col-2 col-md-1 p-0');
-        saveButtonEl = $('<button>').addClass('saveBtn').text('Save');
+        let saveButtonEl = $('<button>').addClass('saveBtn').text('Save');
         saveTdEl.append(saveButtonEl);
 
         // append all td elements to row
@@ -57,8 +62,23 @@ function createSchedule() {
         currentHour.add(1, "hours");
     }
 
+    // use event delegation on entire table to listen for click on save button
+    scheduleTableEl.on('click', '.saveBtn', handleSaveButton);
+
     // append table to containe 
     scheduleContainerEl.append(scheduleTableEl);
+}
+
+function handleSaveButton(event) {
+    event.preventDefault();
+    const buttonClicked = $(event.target);
+
+    // get description by first getting parent td, it's prev sibling, and finding child description
+    const descEl = buttonClicked.parent().prev().children('.description');
+    console.log('desc: ' + descEl.val());
+    console.log('desc: ' + descEl.attr('data-time'));
+    localStorage.setItem(descEl.attr('data-time'), descEl.val().trim());
+
 }
 
 init();
