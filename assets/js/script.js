bc32c6c7060df2d382a151a7d6a2ca89efd77243
Furhan Samani <contact@furhan.dev>
2022-05-07 01:32:31 -0700

    change lets to consts, update comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,18 +20,18 @@ function renderSchedule() {
     // set the number of hours in a work day (8 + 1 hour for lunch)
     const workHours = 9;
     // create a variable to keep track of current hour in for loop
-    let currentHour = moment().set('hour', 9);
+    const currentHour = moment().set('hour', 9);
     // create a table to append each hour row into 
     scheduleTableEl = $('<table>').addClass('table');
     // loop over all work hours and add row
     for (let i = 0; i < workHours; i++) {
         // create a row
-        let rowEl = $('<tr>').addClass('row time-block');
+        const rowEl = $('<tr>').addClass('row time-block');
         // create hour td
-        let hourTdEl = $('<td>').addClass('hour col-2 col-md-1').text(currentHour.format('hA'));
+        const hourTdEl = $('<td>').addClass('hour col-2 col-md-1').text(currentHour.format('hA'));
 
         // create desc container td with a data-time attribute to be set to the currentHour in unix time
-        let descTdEl = $('<td>').addClass('col-8 col-md-10 p-0');
+        const descTdEl = $('<td>').addClass('col-8 col-md-10 p-0');
         // color background based on current hour
         if (currentHour.isBefore(rightNow, 'hour')) {
             descTdEl.addClass('past');
@@ -42,7 +42,7 @@ function renderSchedule() {
         }
 
         // create desc text area element
-        let descEl = $('<textarea>').addClass('description').attr('data-time', currentHour.format('YYYY-MM-DD HH'));
+        const descEl = $('<textarea>').addClass('description').attr('data-time', currentHour.format('YYYY-MM-DD HH'));
         // set the description if previously saved
         const desc = localStorage.getItem(currentHour.format('YYYY-MM-DD HH'));
         if (desc != null) {
@@ -51,8 +51,8 @@ function renderSchedule() {
         descTdEl.append(descEl);
 
         // create save button td
-        let saveTdEl = $('<td>').addClass('col-2 col-md-1 p-0');
-        let saveButtonEl = $('<button>').addClass('saveBtn').text('Save');
+        const saveTdEl = $('<td>').addClass('col-2 col-md-1 p-0');
+        const saveButtonEl = $('<button>').addClass('saveBtn').text('Save');
         saveTdEl.append(saveButtonEl);
 
         // append all td elements to row
@@ -85,8 +85,8 @@ function handleSaveButton(event) {
 
     // get description by first getting parent td, it's prev sibling, and finding child description
     const descEl = buttonClicked.parent().prev().children('.description');
-    console.log('desc: ' + descEl.val());
-    console.log('desc: ' + descEl.attr('data-time'));
+
+    // write the description to local storage for that hour
     localStorage.setItem(descEl.attr('data-time'), descEl.val().trim());
 
 }
