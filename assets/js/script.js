0acd0a2b1c0f9fcd79f895b2c3b7c8b3cb4e145e
Furhan Samani <contact@furhan.dev>
2022-05-06 22:56:59 -0700

    add current time and logic to shade background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 const scheduleContainerEl = $('.container');
+var rightNow;
 var scheduleTableEl;
 
 function init() {
+    // set current date/time
+    rightNow = moment();
+    $('#currentDay').text(rightNow.format('dddd, MMMM Do'));
     // build initial work day schedule
     createSchedule();
 }
@@ -11,18 +15,27 @@ function createSchedule() {
     // set the number of hours in a work day (8 + 1 hour for lunch)
     const workHours = 9;
     // create a variable to keep track of current hour in for loop
-    let currentHour = moment("09:00", "hh:mm");
+    let currentHour = moment().set('hour', 9);
     // create a table to append each hour row into 
     scheduleTableEl = $('<table>').addClass('table');
-
-    // loop to create a row for each work hour
+    // loop over all work hours and add row
     for (let i = 0; i < workHours; i++) {
         // create a row
         let rowEl = $('<tr>').addClass('row');
         // create hour td
-        let hourTdEl = $('<td>').addClass('hour col-2 col-md-1 pe-1').text(currentHour.format('hA'));
+        let hourTdEl = $('<td>').addClass('hour col-2 col-md-1').text(currentHour.format('hA'));
+
         // create desc container td
         let descTdEl = $('<td>').addClass('col-8 col-md-10 p-0');
+        // color background based on current hour
+        if (currentHour.isBefore(rightNow, 'hour')) {
+            descTdEl.addClass('past');
+        } else if (currentHour.isAfter(rightNow, 'hour')) {
+            descTdEl.addClass('future');
+        } else {
+            descTdEl.addClass('present');
+        }
+
         // create desc text area element
         let descEL = $('<textarea>').addClass('description');
         descTdEl.append(descEL);
