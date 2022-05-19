35781ea88af462e9831d15db1079ae8687cbbdb6
Furhan Samani <contact@furhan.dev>
2022-05-18 20:36:23 -0700

    update with popovers

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,25 @@ function init() {
     $('#currentDay').text(rightNow.format('dddd, MMMM Do'));
     // build initial work day schedule
     renderSchedule();
+    // initialize all popovers
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        let popover = new bootstrap.Popover(popoverTriggerEl, 
+            { 
+                container: 'body',
+                html: true,
+                sanitize: false,
+                // content: $('#popover-content')[0].innerHTML
+
+            })
+        return popover;
+    });
+
+    // add event handlers
+    scheduleTableEl.on('click', '.description', handleScheduleClick);
+    $('body').on('click', '.save-event', handleSaveEvent)
+
+    console.log("popover length " + $('.popover-content').length)
 }
 
 /**
@@ -26,12 +45,12 @@ function renderSchedule() {
     // loop over all work hours and add row
     for (let i = 0; i < workHours; i++) {
         // create a row
-        const rowEl = $('<tr>').addClass('row time-block');
+        const rowEl = $('<tr>').addClass('row');
         // create hour td
-        const hourTdEl = $('<td>').addClass('hour col-2 col-md-1').text(currentHour.format('hA'));
+        const hourTdEl = $('<td>').addClass('hour col-2 col-md-1').text(currentHour.format('h A'));
 
         // create desc container td with a data-time attribute to be set to the currentHour in unix time
-        const descTdEl = $('<td>').addClass('col-8 col-md-10 p-0');
+        const descTdEl = $('<td>').addClass('col-9 col-md-10 p-0');
         // color background based on current hour
         if (currentHour.isBefore(rightNow, 'hour')) {
             descTdEl.addClass('past');
@@ -39,27 +58,34 @@ function renderSchedule() {
             descTdEl.addClass('future');
         } else {
             descTdEl.addClass('present');
-        }
+        }   
 
         // create desc text area element
-        const descEl = $('<textarea>').addClass('description').attr('data-time', currentHour.format('YYYY-MM-DD HH'));
+        const descButtonEl = $('<button>')
+            .addClass('btn description')
+            .attr('data-bs-toggle', 'popover')
+            .attr('data-time', currentHour.format('YYYY-MM-DD HH'))
+            // .attr('data-bs-content-id', 'popover-content')
+            .attr('title', "<p class='text-center my-1'>Create New Event</p>") 
+            .attr('data-bs-content', createPopoverContent(currentHour, ""));
         // set the description if previously saved
         const desc = localStorage.getItem(currentHour.format('YYYY-MM-DD HH'));
         if (desc != null) {
-            descEl.text(desc);
+            // const descParsedEl = $.parseHTML(JSON.parse(desc));
+            descButtonEl.attr('title', "<p class='text-center my-1'>" + currentHour.format('h A') + " Event</p>") 
+            // let popoverContent = createPopoverContent(currentHour, descParsedEl[0].innerText); 
+            let popoverContent = createPopoverContent(currentHour, desc); 
+            descButtonEl.attr('data-bs-content', popoverContent);
+            // descButtonEl.append(JSON.parse(desc));
+            const eventDivEl = $('<div>').addClass('event d-flex');
+            descButtonEl.append(eventDivEl.text(desc));
         }
-        descTdEl.append(descEl);
-
-        // create save button td
-        const saveTdEl = $('<td>').addClass('col-2 col-md-1 p-0');
-        const saveButtonEl = $('<button>').addClass('saveBtn').text('Save');
-        saveTdEl.append(saveButtonEl);
+        descTdEl.append(descButtonEl);
 
         // append all td elements to row
         rowEl.append(
             hourTdEl,
-            descTdEl,
-            saveTdEl);
+            descTdEl);
         
         // append row to table
         scheduleTableEl.append(rowEl);
@@ -68,27 +94,72 @@ function renderSchedule() {
         currentHour.add(1, "hours");
     }
 
-    // use event delegation on entire table to listen for click on save button
-    scheduleTableEl.on('click', '.saveBtn', handleSaveButton);
-
     // append table to containe 
     scheduleContainerEl.append(scheduleTableEl);
 }
 
+// need to create popover on the div that i create inside the hour button
+// need to check if div already exists, if so edit, otherwise create new
+// update div popover title based on if event already exists
+
+// prevent popover dismissal
+// double click to create new event or edit existing one
+// single click to focus event
+
+function handleSaveEvent(event) {
+    event.preventDefault();
+    const textAreaEl = $(this).parent().prev();
+    localStorage.setItem(textAreaEl.attr('data-time'), textAreaEl.val());
+    // reset text area in popover to be empty
+    textAreaEl.val('');
+}
+
 /**
- * Handler for save buttons 
+ * Handler for clicking on the schedule
  * @param {object} event click event
  */
-function handleSaveButton(event) {
+function handleScheduleClick(event) {
     event.preventDefault();
-    const buttonClicked = $(event.target);
+    // hide all other popoevers
+    $("button[data-bs-toggle='popover']").not(this).popover('hide');
+
+    // add event div if it doesn't exist already
+    let eventDivEl = $('<div>').addClass('event d-flex');
+    if ($(this).children().length === 0) {
+        $(this).append(eventDivEl);
+    }
+    
+    // const buttonClicked = $(event.target);
 
     // get description by first getting parent td, it's prev sibling, and finding child description
-    const descEl = buttonClicked.parent().prev().children('.description');
+    // const descEl = $(this).parent().prev().children('.description');
 
     // write the description to local storage for that hour
-    localStorage.setItem(descEl.attr('data-time'), descEl.val().trim());
+    // get parent element
+    // console.log(descEl.attr('data-time'));
 
+    // localStorage.setItem($(this).attr('data-time'), JSON.stringify(eventDivEl[0].outerHTML));
+
+}
+
+function createPopoverContent(currentHour, eventDesc) {
+    const textAreaEL = $('<textarea>')
+            .addClass('form-control my-1')
+            .attr('placeholder', "Enter event details")
+            .attr('data-time', currentHour.format('YYYY-MM-DD HH'));
+    const buttonContainerEl = $('<div>').addClass('d-flex justify-content-between my-1');
+    const saveBtnEl = $('<button>').addClass('btn btn-primary save-event').text('Save');
+    const cancelBtnEl = $('<button>').addClass('btn btn-danger me-1 cancel-event').text('Cancel');
+    if (eventDesc) {
+        textAreaEL.text(eventDesc);
+        cancelBtnEl.text('Delete')
+    }
+    buttonContainerEl.append(cancelBtnEl, saveBtnEl);
+    buttonContainerEl.on('click', '.save-event', handleSaveEvent);
+//    return $('<div>').addClass("popover-content d-flex flex-column").append(textAreaEL, buttonContainerEl).prop('outerHTML');
+    const contentString = $('<div>').addClass("popover-content d-flex flex-column").append(textAreaEL, buttonContainerEl).prop('outerHTML');
+//    console.log("content string: " + contentString);
+    return contentString;
 }
 
 // call init function on load
